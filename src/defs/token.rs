32c78f2a85eb78b11ae54273c7d614c9d6c7f720
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-10 17:55:01 +0300

    Expanded upon the tokens and added some essential missing operators

--- src/defs/token.rs
+++ src/defs/token.rs
@@ -17,7 +17,14 @@ pub enum TokenType {
     Minus,
     Asterisk,
     Slash,
+    Modulo,
     Equals,
+    NotEquals,
+    GreaterThanEqual,
+    LesserThanEqual,
+    Or,
+    And,
+    Not,
     Semicolon,
     LParen,
     RParen,
@@ -30,8 +37,34 @@ pub enum TokenType {
     Colon,
     ReturnIndicator,
     QuestionMark,
+    ArrowOpen,
+    ArrowClose,
 
     Comment,
+
+    // Reserved Keywords
+    For,
+    If,
+    Else,
+    ElseIf,
+    Const,
+    Program,
+    Variables,
+    String,
+    Integer,
+    Float,
+    Dynamic,
+    Boolean,
+    DefineFunction,
+    DefineProgram,
+    Function,
+    While,
+    From,
+    To,
+    WithStep,
+    Mutable,
+    Type,
+    Typeof,
 
     // Special Stuff Regarding Mascal
     Unknown,
@@ -42,12 +75,39 @@ pub static TOKEN_REGEX_MAP: Lazy<TokenRegexMap> = Lazy::new(|| {
     let mut map: TokenRegexMap = Vec::new();
     map.push((Regex::new(r"//.*").unwrap(), TokenType::Comment));
     map.push((Regex::new(r"NULL").unwrap(), TokenType::NULL));
+    map.push((Regex::new(r"FOR|for|For").unwrap(), TokenType::For));
+    map.push((Regex::new(r"If|if|IF").unwrap(), TokenType::If));
+    map.push((Regex::new(r"Else|else|ELSE").unwrap(), TokenType::Else));
+    map.push((Regex::new(r"Elif|elif|ELIF").unwrap(), TokenType::ElseIf));
+    map.push((Regex::new(r"Const|const|CONST").unwrap(), TokenType::Const));
+    map.push((Regex::new(r"PROGRAM|program|Program").unwrap(), TokenType::Program));
+    map.push((Regex::new(r"VARIABLES|variables|Variables").unwrap(), TokenType::Variables));
+    map.push((Regex::new(r"String|string|STRING").unwrap(), TokenType::String));
+    map.push((Regex::new(r"INTEGER|integer|Integer").unwrap(), TokenType::Integer));
+    map.push((Regex::new(r"FLOAT|float|Float").unwrap(), TokenType::Float));
+    map.push((Regex::new(r"Dynamic|DYNAMIC|dynamic").unwrap(), TokenType::Dynamic));
+    map.push((Regex::new(r"Boolean|BOOLEAN|boolean").unwrap(), TokenType::Boolean));
+    map.push((Regex::new(r"DEFINE_FUNCTION|define_function|Define_Function").unwrap(), TokenType::DefineFunction));
+    map.push((Regex::new(r"DEFINE_PROGRAM|define_program|Define_Program").unwrap(), TokenType::DefineProgram));
+    map.push((Regex::new(r"FUNCTION|Function|function").unwrap(), TokenType::Function));
+    map.push((Regex::new(r"While|while|WHILE").unwrap(), TokenType::While));
+    map.push((Regex::new(r"From|from|FROM").unwrap(), TokenType::From));
+    map.push((Regex::new(r"To|to|TO").unwrap(), TokenType::To));
+    map.push((Regex::new(r"Mut|mut|MUT").unwrap(), TokenType::Mutable));
+    map.push((Regex::new(r"with_step|With_Step|WITH_STEP").unwrap(), TokenType::WithStep));
+    map.push((Regex::new(r"with_step|With_Step|WITH_STEP").unwrap(), TokenType::WithStep));
+    map.push((Regex::new(r"typeof|TypeOf|TYPEOF").unwrap(), TokenType::Typeof));
+    map.push((Regex::new(r"type|Type|TYPE").unwrap(), TokenType::Type));
+    map.push((Regex::new(r"and|AND|And").unwrap(), TokenType::And));
+    map.push((Regex::new(r"or|OR|Or").unwrap(), TokenType::Or));
+    map.push((Regex::new(r"not|NOT|Not").unwrap(), TokenType::Not));
     map.push((Regex::new(r"(?:^|[^0-9.])(\d+\.\d*|\d*\.\d+)").unwrap(), TokenType::FloatLiteral));
     map.push((Regex::new(r"(\d+)").unwrap(), TokenType::IntegerLiteral));
     map.push((Regex::new("\"[^\"]*\"").unwrap(), TokenType::StringLiteral));
     map.push((Regex::new(r"->").unwrap(), TokenType::ReturnIndicator));
     map.push((Regex::new(r"\+").unwrap(), TokenType::Plus));
     map.push((Regex::new(r"-").unwrap(), TokenType::Minus));
+    map.push((Regex::new(r"%").unwrap(), TokenType::Modulo));
     map.push((Regex::new(r"\*").unwrap(), TokenType::Asterisk));
     map.push((Regex::new(r"\?").unwrap(), TokenType::QuestionMark));
     map.push((Regex::new(r"/").unwrap(), TokenType::Slash));
@@ -62,8 +122,13 @@ pub static TOKEN_REGEX_MAP: Lazy<TokenRegexMap> = Lazy::new(|| {
     map.push((Regex::new(r"\.").unwrap(), TokenType::Dot));
     map.push((Regex::new(r",").unwrap(), TokenType::Comma));
     map.push((Regex::new(r":").unwrap(), TokenType::Colon));
+    map.push((Regex::new(r"!=").unwrap(), TokenType::NotEquals));
     map.push((Regex::new(r"=").unwrap(), TokenType::Equals));
     map.push((Regex::new(r"<-").unwrap(), TokenType::VariableInitializer));
+    map.push((Regex::new(r">=").unwrap(), TokenType::GreaterThanEqual));
+    map.push((Regex::new(r"<=").unwrap(), TokenType::LesserThanEqual));
+    map.push((Regex::new(r"<").unwrap(), TokenType::ArrowOpen));
+    map.push((Regex::new(r">").unwrap(), TokenType::ArrowClose));
     map.push((Regex::new(r"^[a-zA-Z_][a-zA-Z0-9_]*").unwrap(), TokenType::Identifier));
 
     map
