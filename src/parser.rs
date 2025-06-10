67665b0778fe73439f02c8f534db8d54a3634cc0
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-10 17:54:23 +0300

    Removed some of the stuff for parsing and left it empty, it is WIP

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,5 @@
 use crate::defs::token::Token;
 
-pub enum Expression<'a> {
-    Atom(Token<'a>),
-    Operation(Token<'a>, Vec<Expression<'a>>),
-    
-}
-
 pub fn parse(tokens: Vec<Token>) {
-    
+
 }
