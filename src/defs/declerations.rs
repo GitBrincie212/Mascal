490f589f2c6344ebf9711bd9c9187ca23697e38c
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-11 21:37:44 +0300

    Made the fields public and removed the derivation of PartialEq

--- src/defs/declerations.rs
+++ src/defs/declerations.rs
@@ -1,9 +1,11 @@
 use crate::defs::expressions::MascalExpression;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, Clone)]
 pub struct MascalVariableInitialDeclaration {
-    name: String,
-    is_constant: bool,
-    is_nullable: bool,
-    initial_value: MascalExpression,
+    pub name: String,
+    pub is_constant: bool,
+    pub is_nullable: bool,
+    pub dimensions: Vec<MascalExpression>,
+    pub initial_value: Option<MascalExpression>,
+    pub is_dynamic_array: Vec<bool>,
 }
