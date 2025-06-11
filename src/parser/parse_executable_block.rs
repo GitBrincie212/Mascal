6ab688903b18fe84d338cad0663adeea0b8a2947
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-11 21:40:33 +0300

    Added parsing for an executable block(will tweak it to support functions as well)

---
+++ src/parser/parse_executable_block.rs
@@ -0,0 +1,25 @@
+use crate::defs::errors::MascalError;
+use crate::defs::statements::MascalStatement;
+use crate::defs::token::TokenType;
+use crate::parser::parse_statement::parse_statement;
+use crate::parser::Parser;
+use crate::parser::utils::{locate_block, run_per_statement};
+
+pub fn parse_executable(inner_parser: Parser) -> Result<Vec<MascalStatement>, MascalError> {
+    let mut statements: Vec<MascalStatement> = Vec::new();
+    let program_parser = locate_block(
+        inner_parser,
+        TokenType::Program,
+        "PROGRAM",
+        &[],
+        &[],
+    )?.unwrap();
+
+    run_per_statement(&program_parser, |token_sequence| {
+        let stmt: MascalStatement = parse_statement(token_sequence)?;
+        statements.push(stmt);
+        return Ok(());
+    })?;
+    
+    Ok(statements)
+}
