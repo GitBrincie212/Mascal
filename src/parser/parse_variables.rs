1d22a608ad302bd76f7bd9b24389739fb006e5af
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-11 23:04:17 +0300

    Eliminated dead code and used the renamed token sequence struct

--- src/parser/parse_variables.rs
+++ src/parser/parse_variables.rs
@@ -3,15 +3,15 @@ use crate::defs::declerations::MascalVariableInitialDeclaration;
 use crate::defs::errors::{MascalError, MascalErrorType};
 use crate::defs::token::{Token, TokenType};
 use crate::parser::parse_variable_decl::parse_variable_decl;
-use crate::parser::Parser;
+use crate::parser::TokenSequence;
 use crate::parser::utils::{extract_braced_block, locate_block_from, run_per_statement};
 
 pub fn parse_variable_type_block<'a>(
     tokens: &Vec<Token>, token_type: TokenType, block_name: &'static str
 ) -> Result<Vec<MascalVariableInitialDeclaration>, MascalError> {
-    let var_type_parser: Parser<'_> = locate_block_from(
+    let var_type_parser: TokenSequence<'_> = locate_block_from(
         tokens, token_type, block_name, &[], &[]
-    )?.unwrap_or(Parser::new(vec![]));
+    )?.unwrap_or(TokenSequence::new(vec![]));
 
     let mut variable_initialization: Vec<MascalVariableInitialDeclaration> = Vec::new();
     let final_token_sequence: Vec<Token> = run_per_statement(&var_type_parser, |token_sequence| {
@@ -45,11 +45,11 @@ pub fn parse_variable_type_block<'a>(
     Ok(variable_initialization)
 }
 
-pub fn parse_variable_block(parser: &Parser) -> Result<VariableBlock, MascalError> {
-    for (index, token) in (&parser.tokens).iter().enumerate() {
+pub fn parse_variable_block(token_sequence: &TokenSequence) -> Result<VariableBlock, MascalError> {
+    for (index, token) in (&token_sequence.tokens).iter().enumerate() {
         if token.token_type != TokenType::Variables { continue }
         let subset_parser = extract_braced_block(
-            parser.subsection_from(index + 1..),
+            token_sequence.subsection_from(index + 1..),
             "VARIABLES",
             &[TokenType::Integer, TokenType::Float, TokenType::String, TokenType::Boolean, TokenType::Dynamic, TokenType::Type],
             &[],
