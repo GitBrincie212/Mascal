0c2ab6d24808f10acb53ccb5a993774b12ec94e9
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-11 21:42:07 +0300

    Added parsing for variable type blocks(like INTEGER, STRING... etc)

---
+++ src/parser/parse_variables.rs
@@ -0,0 +1,100 @@
+use crate::defs::blocks::VariableBlock;
+use crate::defs::declerations::MascalVariableInitialDeclaration;
+use crate::defs::errors::{MascalError, MascalErrorType};
+use crate::defs::token::{Token, TokenType};
+use crate::parser::parse_variable_decl::parse_variable_decl;
+use crate::parser::Parser;
+use crate::parser::utils::{extract_braced_block, locate_block_from, run_per_statement};
+
+pub fn parse_variable_type_block<'a>(
+    tokens: &Vec<Token>, token_type: TokenType, block_name: &'static str
+) -> Result<Vec<MascalVariableInitialDeclaration>, MascalError> {
+    let var_type_parser: Parser<'_> = locate_block_from(
+        tokens, token_type, block_name, &[], &[]
+    )?.unwrap_or(Parser::new(vec![]));
+
+    let mut variable_initialization: Vec<MascalVariableInitialDeclaration> = Vec::new();
+    let final_token_sequence: Vec<Token> = run_per_statement(&var_type_parser, |token_sequence| {
+        if token_sequence.is_empty() {
+            return Ok(());
+        } else if token_sequence.len() == 1 {
+            variable_initialization.push(MascalVariableInitialDeclaration {
+                name: token_sequence.get(0).unwrap().value.to_string(),
+                initial_value: None,
+                is_constant: false,
+                is_dynamic_array: Vec::new(),
+                dimensions: Vec::new(),
+                is_nullable: false,
+            });
+            return Ok(());
+        }
+        let variable_decl = parse_variable_decl(token_sequence)?;
+        variable_initialization.push(variable_decl);
+        return Ok(());
+    })?;
+
+    if !final_token_sequence.is_empty() {
+        return Err(MascalError {
+            error_type: MascalErrorType::ParserError,
+            line: final_token_sequence.first().unwrap().line,
+            character: final_token_sequence.first().unwrap().start,
+            source: String::from("Unexpected characters for variable declaration, perhaps it hasn't been closed with a semicolon?")
+        });
+    }
+
+    Ok(variable_initialization)
+}
+
+pub fn parse_variable_block(parser: &Parser) -> Result<VariableBlock, MascalError> {
+    for (index, token) in (&parser.tokens).iter().enumerate() {
+        if token.token_type != TokenType::Variables { continue }
+        let subset_parser = extract_braced_block(
+            parser.subsection_from(index + 1..),
+            "VARIABLES",
+            &[TokenType::Integer, TokenType::Float, TokenType::String, TokenType::Boolean, TokenType::Dynamic, TokenType::Type],
+            &[],
+        )?;
+
+        let integer_tokens: Vec<MascalVariableInitialDeclaration> = parse_variable_type_block(
+            &subset_parser.tokens, TokenType::Integer, "INTEGER"
+        )?;
+
+        let float_tokens: Vec<MascalVariableInitialDeclaration> = parse_variable_type_block(
+            &subset_parser.tokens, TokenType::Float, "FLOAT"
+        )?;
+
+        let string_tokens: Vec<MascalVariableInitialDeclaration> = parse_variable_type_block(
+            &subset_parser.tokens, TokenType::String, "STRING"
+        )?;
+
+        let boolean_tokens: Vec<MascalVariableInitialDeclaration> = parse_variable_type_block(
+            &subset_parser.tokens, TokenType::Boolean, "BOOLEAN"
+        )?;
+
+        let dynamic_tokens: Vec<MascalVariableInitialDeclaration> = parse_variable_type_block(
+            &subset_parser.tokens, TokenType::Dynamic, "DYNAMIC"
+        )?;
+
+        let type_tokens: Vec<MascalVariableInitialDeclaration> = parse_variable_type_block(
+            &subset_parser.tokens, TokenType::Type, "TYPE"
+        )?;
+
+        return Ok(VariableBlock {
+            integers: integer_tokens,
+            floats: float_tokens,
+            strings: string_tokens,
+            booleans: boolean_tokens,
+            dynamics: dynamic_tokens,
+            types: type_tokens,
+        });
+    }
+
+    Ok(VariableBlock {
+        integers: vec![],
+        floats: vec![],
+        strings: vec![],
+        booleans: vec![],
+        dynamics: vec![],
+        types: vec![],
+    })
+}
