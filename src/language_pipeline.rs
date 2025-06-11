859eb9031aa2ba0b2bd60055906947e9b59f4722
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-11 21:38:59 +0300

    Added parsing to the language pipeline

--- src/language_pipeline.rs
+++ src/language_pipeline.rs
@@ -1,6 +1,8 @@
+use crate::defs::blocks::ScopedBlocks;
 use crate::defs::errors::{MascalErrorType, MascalError};
 use crate::defs::token::{Token, TokenType};
 use crate::lexer;
+use crate::parser::Parser;
 
 pub fn trigger_pipeline(contents: String) {
     let tokens: Vec<Token> = lexer::tokenize(&*contents);
@@ -15,4 +17,12 @@ pub fn trigger_pipeline(contents: String) {
         });
         return;
     }
+    let lang_parser: Parser = Parser::new(tokens);
+    let resulted_blocks: Result<Vec<ScopedBlocks>, MascalError> = lang_parser.parse();
+    if resulted_blocks.is_err() {
+        println!("{}", resulted_blocks.err().unwrap());
+        return;
+    }
+    let blocks: Vec<ScopedBlocks> = resulted_blocks.unwrap();
+    // dbg!(tokens);
 }
