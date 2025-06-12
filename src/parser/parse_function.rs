be204ea5699398cf526f4f37c49c4674af058a89
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-12 13:04:26 +0300

    Expanded upon the parsing of the function and fully finished it

--- src/parser/parse_function.rs
+++ src/parser/parse_function.rs
@@ -1,7 +1,158 @@
-use crate::defs::errors::MascalError;
-use crate::defs::token::Token;
+use crate::defs::blocks::{ExecutionBlock, MascalParameter, ScopedBlocks};
+use crate::defs::errors::{MascalError, MascalErrorType};
+use crate::defs::expressions::MascalExpression;
+use crate::defs::token::{Token, TokenType};
+use crate::defs::types::MascalType;
+use crate::parser::parse_executable_block::parse_executable;
+use crate::parser::parse_expression::parse_expression;
+use crate::parser::parse_variables::parse_variable_block;
 use crate::parser::TokenSequence;
+use crate::parser::utils::{extract_braced_block, parse_array_type};
 
-pub fn parse_function<'a>(preceding_token: &Token, parser: TokenSequence<'a>) -> Result<&'a [Token<'a>], MascalError> {
-    Ok(&[])
+fn get_parameters_of_func(
+    token_sequence: &TokenSequence, mut curr_index: usize
+) -> Result<(Vec<MascalParameter>, usize), MascalError> {
+    if !token_sequence.is_of(TokenType::OpenParen, curr_index) {
+        return Ok((Vec::new(), curr_index));
+    } else if token_sequence.is_of(TokenType::CloseParen, curr_index + 1) {
+        return Ok((Vec::new(), curr_index + 2));
+    }
+    let mut is_mutable: bool = false;
+    let mut parameter_name: Option<String> = None;
+    let mut parameters: Vec<MascalParameter> = Vec::new();
+    while !token_sequence.is_of(TokenType::CloseParen, curr_index) {
+        if curr_index >= token_sequence.tokens.len() {
+            return Err(MascalError {
+                error_type: MascalErrorType::ParserError,
+                line: token_sequence.first_token().line,
+                character: token_sequence.first_token().start,
+                source: String::from("Parameters have not been closed with a parenthesis")
+            });
+        }
+        let curr_token = token_sequence.acquire_token(curr_index);
+        let next_token_type = &token_sequence.get_token(curr_index + 1)
+            .unwrap_or(curr_token)
+            .token_type;
+        if curr_token.token_type == TokenType::Identifier {
+            parameter_name = Some(curr_token.value.to_string());
+        } else if curr_token.token_type == TokenType::Mutable {
+            if parameter_name.is_some() {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ParserError,
+                    line: curr_token.line,
+                    character: curr_token.start,
+                    source: String::from("the keyword MUT should be followed before the variable's name")
+                })
+            } else if is_mutable {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ParserError,
+                    line: curr_token.line,
+                    character: curr_token.start,
+                    source: String::from("the MUT keyword cannot stack on top of one variable")
+                })
+            }
+            is_mutable = true;
+        }
+        if next_token_type == &TokenType::CloseParen || curr_token.token_type == TokenType::Comma {
+            if parameter_name.is_none() {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ParserError,
+                    line: curr_token.line,
+                    character: curr_token.start,
+                    source: String::from("expected to define parameter name")
+                })
+            }
+            parameters.push(MascalParameter {
+                name: parameter_name.unwrap(),
+                is_mutable
+            });
+            parameter_name = None;
+            is_mutable = false;
+        }
+        curr_index += 1;
+    }
+
+    Ok((parameters, curr_index + 1))
+}
+
+// TODO: Find the bug that causes the tokens list to be empty
+
+pub fn parse_function(token_sequence: TokenSequence) -> Result<ScopedBlocks, MascalError> {
+    let mut curr_index: usize = 0;
+    let name: String;
+    if token_sequence.is_of(TokenType::Identifier, curr_index) {
+        name = token_sequence.first_token().value.to_string();
+        curr_index += 1;
+    } else {
+        return Err(MascalError {
+            error_type: MascalErrorType::ParserError,
+            line: token_sequence.first_token().line,
+            character: token_sequence.first_token().start,
+            source: String::from("Expected a identifier for the function name")
+        })
+    }
+
+    let returned_tuple = get_parameters_of_func(&token_sequence, curr_index)?;
+    let parameters: Vec<MascalParameter> = returned_tuple.0;
+    curr_index = returned_tuple.1;
+
+    let mut return_type: Option<MascalType> = None;
+    if token_sequence.is_of(TokenType::ReturnIndicator, curr_index) {
+        curr_index += 1;
+        let curr_token: &Token = token_sequence.acquire_token(curr_index);
+        return_type = Some(match curr_token.token_type {
+            TokenType::Integer => MascalType::Integer,
+            TokenType::Float => MascalType::Float,
+            TokenType::Boolean => MascalType::Boolean,
+            TokenType::Dynamic => MascalType::Dynamic,
+            TokenType::String => MascalType::String,
+            TokenType::Type => MascalType::Type,
+            _ => {return Err(MascalError {
+                error_type: MascalErrorType::ParserError,
+                line: curr_token.line,
+                character: curr_token.start,
+                source: String::from("Expected a specific type to be returned and got nothing")
+            })}
+        });
+        curr_index += 1;
+        let mut dimensions: Vec<(MascalExpression, bool)> = Vec::new();
+        curr_index = parse_array_type(&token_sequence.tokens, curr_index, |toks, is_dynamic | {
+            dimensions.push((parse_expression(&toks.to_vec())?, is_dynamic));
+            Ok(())
+        }, vec![TokenType::OpenBrace])?;
+        if !dimensions.is_empty() {
+            for (dimension, is_dynamic) in dimensions {
+                if is_dynamic {
+                    return_type = Some(MascalType::DynamicArray {
+                        array_type: Box::new(return_type.unwrap()),
+                        initial_size: dimension
+                    });
+                    continue;
+                }
+                return_type = Some(MascalType::StaticArray {
+                    array_type: Box::new(return_type.unwrap()),
+                    size: dimension
+                });
+            }
+        }
+    }
+
+    let inner_token_sequence = extract_braced_block(
+        token_sequence.subsection_from(curr_index..),
+        "DEFINE_FUNCTION",
+        &[TokenType::Variables],
+        &[TokenType::Implementation],
+    )?;
+    let variable_block = parse_variable_block(&inner_token_sequence)?;
+    let program_body = parse_executable(inner_token_sequence)?;
+
+    Ok(ScopedBlocks::FUNCTION {
+        parameters,
+        name,
+        return_type,
+        execution_block: ExecutionBlock {
+            variables: variable_block,
+            body: program_body,
+        }
+    })
 }
