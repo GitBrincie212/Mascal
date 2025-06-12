60e81dbd6528681c6a250191d2986b51b07f0a37
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-12 13:03:51 +0300

    Used the new utility function for parsing array types

--- src/parser/parse_variable_decl.rs
+++ src/parser/parse_variable_decl.rs
@@ -4,6 +4,7 @@ use crate::defs::expressions::MascalExpression;
 use crate::defs::literal::MascalLiteral;
 use crate::defs::token::{Token, TokenType};
 use crate::parser::parse_expression::parse_expression;
+use crate::parser::utils::parse_array_type;
 
 pub fn parse_variable_decl<'a>(
     tokens: &'a Vec<Token<'a>>
@@ -32,86 +33,17 @@ pub fn parse_variable_decl<'a>(
             source: String::from("Expected a variable name"),
         });
     }
-
-    let mut bracket_depth: usize = 0;
-    let mut arrow_depth: usize = 0;
-    let mut last_token: &Token = &tokens[curr_index];
-    let first_token: &Token = &tokens[curr_index];
-
-    let mut open_index: usize = 0;
-    let mut is_curr_array_dynamic: Option<bool> = None;
-    let is_array_open: bool = first_token.token_type == TokenType::OpenBracket
-        || first_token.token_type == TokenType::OpenArrow;
-    while curr_index < tokens.len() && is_array_open {
-        let mut token_sequence: Vec<&Token> = Vec::new();
-        let token: &Token = &tokens[curr_index];
-        match tokens[curr_index].token_type {
-            TokenType::OpenBracket => {
-                if bracket_depth == 0 && is_curr_array_dynamic.is_none() {
-                    open_index = curr_index + 1;
-                    is_curr_array_dynamic = Some(false);
-                }
-                bracket_depth += 1;
-            }
-
-            TokenType::CloseBracket => {
-                if bracket_depth == 1 && !is_curr_array_dynamic.unwrap_or(true) {
-                    let tokens_inside = &tokens[open_index..curr_index];
-                    dimensions.push(parse_expression(&tokens_inside.to_vec())?);
-                    is_dynamic_array.push(false);
-                    is_curr_array_dynamic = None;
-                }
-                bracket_depth -= 1;
-            }
-
-            TokenType::OpenArrow => {
-                if bracket_depth == 0 && is_curr_array_dynamic.is_none() {
-                    open_index = curr_index + 1;
-                    is_curr_array_dynamic = Some(true);
-                }
-                arrow_depth += 1;
-            }
-
-            TokenType::CloseArrow => {
-                if arrow_depth == 1 && is_curr_array_dynamic.unwrap_or(false) {
-                    let tokens_inside = &tokens[open_index..curr_index];
-                    dimensions.push(parse_expression(&tokens_inside.to_vec())?);
-                    is_dynamic_array.push(true);
-                    is_curr_array_dynamic = None;
-                }
-                arrow_depth -= 1;
-            }
-
-            TokenType::Semicolon | TokenType::VariableInitializer | TokenType::QuestionMark => {
-                last_token = token;
-                break;
-            }
-
-            _ => {
-                if bracket_depth > 0 || arrow_depth > 0 {
-                    token_sequence.push(token);
-                }
-            }
+    
+    curr_index = parse_array_type(tokens, curr_index, |token_sequence, is_dynamic | {
+        if is_dynamic {
+            dimensions.push(parse_expression(&token_sequence.to_vec())?);
+            is_dynamic_array.push(true);
+            return Ok(());
         }
-        last_token = token;
-        curr_index += 1;
-    }
-
-    if bracket_depth != 0 {
-        return Err(MascalError {
-            error_type: MascalErrorType::ParserError,
-            line: last_token.line,
-            character: last_token.start,
-            source: String::from("Bracket has not been closed for array type")
-        })
-    } else if arrow_depth != 0 {
-        return Err(MascalError {
-            error_type: MascalErrorType::ParserError,
-            line: last_token.line,
-            character: last_token.start,
-            source: String::from("Arrow has not been closed for dynamic array type")
-        })
-    }
+        dimensions.push(parse_expression(&token_sequence.to_vec())?);
+        is_dynamic_array.push(false);
+        Ok(())
+    }, vec![TokenType::Semicolon, TokenType::VariableInitializer, TokenType::QuestionMark])?;
 
     if tokens[curr_index].token_type == TokenType::QuestionMark {
         is_nullable = true;
