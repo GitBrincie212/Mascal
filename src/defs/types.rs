378b4464944b15e36658d56f236165daeda2853a
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-12 13:02:34 +0300

    Removed the equal deriving and tweaked the types to support expressions

--- src/defs/types.rs
+++ src/defs/types.rs
@@ -1,17 +1,20 @@
-#[derive(Debug, Clone, PartialEq, Eq)]
+use crate::defs::expressions::MascalExpression;
+
+#[derive(Debug, Clone)]
 pub enum MascalType {
     Integer,
     Float,
     Boolean,
     String,
     Dynamic,
+    Type,
     NULL,
     DynamicArray {
         array_type: Box<MascalType>,
-        initial_size: usize,
+        initial_size: MascalExpression,
     },
     StaticArray {
         array_type: Box<MascalType>,
-        size: usize,
+        size: MascalExpression,
     },
 }
