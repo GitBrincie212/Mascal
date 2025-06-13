9985eedcd61937639860a5a6cc4b33e80bf53c18
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-13 17:21:17 +0300

    Removed NULL as a type and added a way to translate to atomic types

--- src/defs/types.rs
+++ src/defs/types.rs
@@ -1,4 +1,5 @@
 use crate::defs::expressions::MascalExpression;
+use crate::defs::token::TokenType;
 
 #[derive(Debug, Clone)]
 pub enum MascalType {
@@ -8,7 +9,6 @@ pub enum MascalType {
     String,
     Dynamic,
     Type,
-    NULL,
     DynamicArray {
         array_type: Box<MascalType>,
         initial_size: MascalExpression,
@@ -17,4 +17,15 @@ pub enum MascalType {
         array_type: Box<MascalType>,
         size: MascalExpression,
     },
+}
+
+pub fn token_type_to_atom_mascal_type(tt: &TokenType) -> Option<MascalType> {
+    match tt {
+        TokenType::Integer => Some(MascalType::Integer),
+        TokenType::Float => Some(MascalType::Float),
+        TokenType::String => Some(MascalType::String),
+        TokenType::Dynamic => Some(MascalType::Dynamic),
+        TokenType::Type => Some(MascalType::Type),
+        _ => None
+    }
 }
