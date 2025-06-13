82bfa440984cf83e7405d33ece2c2e69f475a306
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-13 22:47:40 +0300

    Extracted all the boilerplate into steps defined by a macro

--- src/parser/parse_expression.rs
+++ src/parser/parse_expression.rs
@@ -1,10 +1,20 @@
 mod parse_primary;
+mod loop_flags;
+mod parse_binary_expression;
+mod parse_callable;
+mod parse_inner_member;
+mod utils;
 
-use crate::defs::binding_power::{get_binding_power_from_bsign, get_binding_power_from_psign, BindingPower};
+use crate::{define_parsing_step};
+use crate::defs::binding_power::{get_binding_power_from_psign, BindingPower};
 use crate::defs::errors::{MascalError, MascalErrorType};
 use crate::defs::expressions::MascalExpression;
-use crate::defs::operators::{token_type_to_binary_operator, token_type_to_unary_operator, MascalBinaryOperators};
+use crate::defs::operators::{token_type_to_unary_operator};
 use crate::defs::token::{Token, TokenType};
+use crate::parser::parse_expression::loop_flags::LoopFlags;
+use crate::parser::parse_expression::parse_binary_expression::parse_binary_expression;
+use crate::parser::parse_expression::parse_callable::parse_callable;
+use crate::parser::parse_expression::parse_inner_member::parse_inner_member;
 use crate::parser::parse_expression::parse_primary::parse_primary;
 
 pub fn parse_expression_internal(
@@ -21,39 +31,14 @@ pub fn parse_expression_internal(
             | Some(TokenType::CloseBracket)
             | Some(TokenType::CloseArrow)
             | Some(TokenType::CloseParen)
-            | None  // end of input
+            | None
             => break 'pratt,
             _ => {}
         }
 
-        let op_tok = tokens.get(*pos).unwrap();
-        let binop = token_type_to_binary_operator(&op_tok.token_type);
-        let bp = if let Some(op) = &binop {
-            get_binding_power_from_bsign(op.clone())
-        } else {
-            break 'pratt;
-        };
-
-        if bp.left_binding_power < min_bp.left_binding_power {
-            break 'pratt;
-        }
-
-        *pos += 1;
-        let op = binop.unwrap();
-        let rhs = parse_expression_internal(
-            tokens,
-            pos,
-            BindingPower {
-                left_binding_power: 0,
-                right_binding_power: bp.right_binding_power,
-            },
-        )?;
-
-        lhs = MascalExpression::BinaryExpression {
-            left:     Box::new(lhs),
-            operator: op,
-            right:    Box::new(rhs),
-        };
+        define_parsing_step!(parse_inner_member, tokens, pos, &min_bp, lhs);
+        define_parsing_step!(parse_callable, tokens, pos, &min_bp, lhs);
+        define_parsing_step!(parse_binary_expression, tokens, pos, &min_bp, lhs);
     }
 
     Ok(lhs)
@@ -62,18 +47,18 @@ pub fn parse_expression_internal(
 pub fn parse_prefix(
     tokens: &[Token], pos: &mut usize,
 ) -> Result<MascalExpression, MascalError> {
-    
+
     let tok =  tokens.get(*pos).ok_or_else(|| MascalError {
         error_type: MascalErrorType::ParserError,
         character: tokens.last().unwrap().start,
         line: tokens.last().unwrap().line,
         source: String::from("Abrupt ending in the expression")
     })?;
-    
+
     if let Some(op) = token_type_to_unary_operator(&tok.token_type) {
         *pos += 1;
-        let bp = get_binding_power_from_psign(op.clone());
-        let rhs = parse_expression_internal(&tokens.to_vec(), pos, bp)?;
+        let bp: BindingPower = get_binding_power_from_psign(op.clone());
+        let rhs: MascalExpression = parse_expression_internal(&tokens.to_vec(), pos, bp)?;
         return Ok(MascalExpression::UnaryExpression {
             operator: op,
             value: Box::new(rhs),
---
+++ src/parser/parse_expression/utils.rs
@@ -0,0 +1,28 @@
+#[macro_export]
+macro_rules! define_parsing_step {
+    ( $func:ident, $tokens:expr, $pos:expr, $lhs_opt:expr ) => {{
+        let (flag, new_lhs) = $func($tokens, $pos, $lhs_opt)?;
+        $lhs_opt = new_lhs;
+        
+        match flag {
+            LoopFlags::BREAK => break,
+            LoopFlags::CONTINUE => {
+                continue;
+            }
+            LoopFlags::NONE => {}
+        }
+    }};
+
+    ( $func:ident, $tokens:expr, $pos:expr, $min_bp:expr, $lhs_opt:expr ) => {{
+        let (flag, new_lhs) = $func($tokens, $pos, $min_bp, $lhs_opt)?;
+        $lhs_opt = new_lhs;
+        
+        match flag {
+            LoopFlags::BREAK => break,
+            LoopFlags::CONTINUE => {
+                continue;
+            }
+            LoopFlags::NONE => {}
+        }
+    }};
+}
