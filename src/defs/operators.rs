f715833bdc95ffff7d379b32c5e63893b6a4cf88
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-13 17:18:17 +0300

    Removed VariableAssign and added Exponentiation as well as added a way to translate token types to operators

--- src/defs/operators.rs
+++ src/defs/operators.rs
@@ -1,7 +1,10 @@
+use crate::defs::token::TokenType;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum MascalUnaryOperators {
     Not,
-    Minus
+    Minus,
+    Typeof
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -19,5 +22,34 @@ pub enum MascalBinaryOperators {
     And,
     Or,
     NotEqual,
-    VariableAssign,
+    Exponentiation
+}
+
+pub fn token_type_to_binary_operator(tt: &TokenType) -> Option<MascalBinaryOperators> {
+    match tt {
+        TokenType::Plus => Some(MascalBinaryOperators::Plus),
+        TokenType::Minus => Some(MascalBinaryOperators::Minus),
+        TokenType::Asterisk => Some(MascalBinaryOperators::Multiply),
+        TokenType::Division => Some(MascalBinaryOperators::Divide),
+        TokenType::Modulo => Some(MascalBinaryOperators::Modulo),
+        TokenType::Exponentiation => Some(MascalBinaryOperators::Exponentiation),
+        TokenType::Equals => Some(MascalBinaryOperators::Equals),
+        TokenType::GreaterThanEqual => Some(MascalBinaryOperators::GreaterThanOrEqual),
+        TokenType::LesserThanEqual => Some(MascalBinaryOperators::LessThanOrEqual),
+        TokenType::And => Some(MascalBinaryOperators::And),
+        TokenType::Or => Some(MascalBinaryOperators::Or),
+        TokenType::NotEquals => Some(MascalBinaryOperators::NotEqual),
+        TokenType::OpenArrow => Some(MascalBinaryOperators::LessThan),
+        TokenType::CloseArrow => Some(MascalBinaryOperators::GreaterThan),
+        _ => None
+    }
+}
+
+pub fn token_type_to_unary_operator(tt: &TokenType) -> Option<MascalUnaryOperators> {
+    match tt {
+        TokenType::Minus =>  Some(MascalUnaryOperators::Minus),
+        TokenType::Not =>  Some(MascalUnaryOperators::Not),
+        TokenType::Typeof =>  Some(MascalUnaryOperators::Typeof),
+        _ => None
+    }
 }
