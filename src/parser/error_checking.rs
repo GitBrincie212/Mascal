d518c77de873c9fbdc6d10a899e2ef69a91a9749
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-14 22:28:54 +0300

    Added a error_checking module for verifying the abstract syntax tree's structure

---
+++ src/parser/error_checking.rs
@@ -0,0 +1,6 @@
+use crate::ast::AbstractSyntaxTree;
+use crate::defs::errors::MascalError;
+
+pub fn verify_ast_structure(abstract_syntax_tree: &AbstractSyntaxTree) -> Result<(), MascalError> {
+    Ok(())
+}
