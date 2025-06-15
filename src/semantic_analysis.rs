d3fc084ba371a62b8dfe809ece24365e9c2534fd
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-15 12:37:25 +0300

    Moved the variable declaration stage function to the semantic analysis function

--- src/semantic_analysis.rs
+++ src/semantic_analysis.rs
@@ -1,11 +1,40 @@
 mod variable_check_stage;
 mod check_parameters_declaration;
 
+use std::collections::HashSet;
 use crate::ast::AbstractSyntaxTree;
-use crate::defs::errors::MascalError;
-use crate::semantic_analysis::variable_check_stage::variable_check_stage;
+use crate::defs::blocks::{ScopedBlocks, VariableBlock};
+use crate::defs::errors::{MascalError, MascalErrorType};
+use crate::semantic_analysis::check_parameters_declaration::check_for_param_declaration;
 
 pub fn conduct_semantic_analysis(abstract_syntax_tree: AbstractSyntaxTree) -> Result<AbstractSyntaxTree, MascalError> {
-    variable_check_stage(&abstract_syntax_tree)?;
+    let mut has_program: bool = false;
+    for block in &abstract_syntax_tree.blocks {
+        let varblock: &VariableBlock = &match block {
+            ScopedBlocks::PROGRAM(exec_block) => {
+                has_program = true;
+                exec_block
+            },
+            ScopedBlocks::FUNCTION {execution_block, parameters, .. } => {
+                check_for_param_declaration(execution_block, parameters)?;
+                execution_block
+            }
+        }.variables;
+        let mut defined_var_names: HashSet<String> = HashSet::new();
+        defined_var_names = variable_check_stage::check_per_variable(&varblock.integers, defined_var_names, true)?;
+        defined_var_names = variable_check_stage::check_per_variable(&varblock.floats, defined_var_names, true)?;
+        defined_var_names = variable_check_stage::check_per_variable(&varblock.strings, defined_var_names, false)?;
+        defined_var_names = variable_check_stage::check_per_variable(&varblock.booleans, defined_var_names, false)?;
+        defined_var_names = variable_check_stage::check_per_variable(&varblock.dynamics, defined_var_names, true)?;
+        variable_check_stage::check_per_variable(&varblock.types, defined_var_names, false)?;
+    }
+    if !has_program {
+        return Err(MascalError {
+            error_type: MascalErrorType::ParserError,
+            character: 0,
+            line: 0,
+            source: String::from("Definition of a program is required in order to execute this file")
+        })
+    }
     Ok(abstract_syntax_tree)
 }
--- src/semantic_analysis/variable_check_stage.rs
+++ src/semantic_analysis/variable_check_stage.rs
@@ -1,12 +1,9 @@
 use std::collections::HashSet;
-use crate::ast::AbstractSyntaxTree;
 use crate::defs::declerations::MascalVariableInitialDeclaration;
 use crate::defs::errors::{MascalError, MascalErrorType};
-use crate::defs::blocks::{ScopedBlocks, VariableBlock};
 use crate::defs::InfinityControl;
-use crate::semantic_analysis::check_parameters_declaration::check_for_param_declaration;
 
-fn check_per_variable(
+pub(crate) fn check_per_variable(
     variable_type: &Vec<MascalVariableInitialDeclaration>, mut defined_var_names: HashSet<String>,
     can_be_numeric: bool
 ) -> Result<HashSet<String>, MascalError> {
@@ -33,36 +30,4 @@ fn check_per_variable(
         defined_var_names.insert(name);
     }
     Ok(defined_var_names)
-}
-
-pub fn variable_check_stage(abstract_syntax_tree: &AbstractSyntaxTree) -> Result<(), MascalError> {
-    let mut has_program: bool = false;
-    for block in &abstract_syntax_tree.blocks {
-        let varblock: &VariableBlock = &match block {
-            ScopedBlocks::PROGRAM(exec_block) => {
-                has_program = true;
-                exec_block
-            },
-            ScopedBlocks::FUNCTION {execution_block, parameters, .. } => {
-                check_for_param_declaration(execution_block, parameters)?;
-                execution_block
-            }
-        }.variables;
-        let mut defined_var_names: HashSet<String> = HashSet::new();
-        defined_var_names = check_per_variable(&varblock.integers, defined_var_names, true)?;
-        defined_var_names = check_per_variable(&varblock.floats, defined_var_names, true)?;
-        defined_var_names = check_per_variable(&varblock.strings, defined_var_names, false)?;
-        defined_var_names = check_per_variable(&varblock.booleans, defined_var_names, false)?;
-        defined_var_names = check_per_variable(&varblock.dynamics, defined_var_names, true)?;
-        check_per_variable(&varblock.types, defined_var_names, false)?;
-    }
-    if !has_program {
-        return Err(MascalError {
-            error_type: MascalErrorType::ParserError,
-            character: 0,
-            line: 0,
-            source: String::from("Definition of a program is required in order to execute this file")
-        })
-    }
-    Ok(())
 }
