8722266d553dece987d7aee07383a23d31269882
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-16 21:50:14 +0300

    Rewrote the macro code to be more inlined with upcoming versions of rust and changed some errors for clarity

--- src/runtime/value_operations.rs
+++ src/runtime/value_operations.rs
@@ -12,7 +12,7 @@ macro_rules! error_float_overflow {
                 "Float has been overflowed which was caused by the addition of {:?} and {:?}",
                 $l, $r
             ),
-        });
+        })
     };
 }
 
@@ -23,7 +23,7 @@ macro_rules! unsupported_operation_error {
             line: 0,
             error_type: MascalErrorType::UndefinedOperation,
             source: format!(
-                "Cannot operate between the types {:?} and {:?}",
+                "Cannot operate between the values {:?} and {:?}",
                 $left.as_string(),
                 $right.as_string()
             ),
@@ -33,93 +33,87 @@ macro_rules! unsupported_operation_error {
 
 macro_rules! define_arithmetic_fn {
     (
-        $fn_name:ident, $intmeth:ident, $floatop:tt,
-        $( $extra_pat:pat => $extra_expr:expr ),* $(,)?
+        $fn_name:expr, $left: expr, $right:expr, $infinity_control: expr, 
+        $intmeth:ident, $floatop:tt, $( $extra_pat:pat => $extra_expr:expr ),* $(,)?
     ) => {
-        impl MascalValue {
-            pub fn $fn_name(
-                left: MascalValue,
-                right: MascalValue,
-                infinity_control: &InfinityControl
-            ) -> Result<MascalValue, MascalError> {
-                match (left, right) {
-                    (MascalValue::Integer(l), MascalValue::Integer(r)) => {
-                        Ok(MascalValue::Integer(l.$intmeth(r, infinity_control)?))
-                    }
+            match ($left, $right) {
+                (MascalValue::Integer(l), MascalValue::Integer(r)) => {
+                    Ok(MascalValue::Integer(l.$intmeth(r, $infinity_control)?))
+                }
 
-                    (l @ MascalValue::Float(_), MascalValue::Integer(r)) => {
-                        $fn_name(l, MascalValue::Float(r.as_f64()), infinity_control)
-                    }
+                (l @ MascalValue::Float(_), MascalValue::Integer(r)) => {
+                    $fn_name(l, MascalValue::Float(r.as_f64()), $infinity_control)
+                }
 
-                    (MascalValue::Integer(l), r @ MascalValue::Float(_)) => {
-                        $fn_name(MascalValue::Float(l.as_f64()), r, infinity_control)
-                    }
+                (MascalValue::Integer(l), r @ MascalValue::Float(_)) => {
+                    $fn_name(MascalValue::Float(l.as_f64()), r, $infinity_control)
+                }
 
-                    (MascalValue::Float(l), MascalValue::Float(r)) => {
-                        match &infinity_control {
-                            &InfinityControl::AllowInfinityExtra => {
-                                Ok(MascalValue::Float(l $floatop r))
+                (MascalValue::Float(l), MascalValue::Float(r)) => {
+                    match &$infinity_control {
+                        &InfinityControl::AllowInfinityExtra => {
+                            Ok(MascalValue::Float(l $floatop r))
+                        }
+
+                        &InfinityControl::AllowInfinity
+                        | &InfinityControl::DisallowInfinity => {
+                            if $infinity_control == &InfinityControl::DisallowInfinity && (l.is_infinite() || r.is_infinite()) {
+                                return error_float_overflow!(l, r);
                             }
-
-                            &InfinityControl::AllowInfinity
-                            | &InfinityControl::DisallowInfinity => {
-                                if infinity_control == &InfinityControl::DisallowInfinity && (l.is_infinite() || r.is_infinite()) {
-                                    return error_float_overflow!(l, r);
-                                }
-                                let res = l $floatop r;
-                                if res.is_infinite() && !l.is_infinite() && !r.is_infinite() {
-                                    return error_float_overflow!(l, r);
-                                }
-                                Ok(MascalValue::Float(res))
+                            let res = l $floatop r;
+                            if res.is_infinite() && !l.is_infinite() && !r.is_infinite() {
+                                return error_float_overflow!(l, r);
                             }
+                            Ok(MascalValue::Float(res))
                         }
                     }
-
-                    $( $extra_pat => $extra_expr, )*
-
-                    (l, r) => unsupported_operation_error!(l, r),
                 }
+
+                $( $extra_pat => $extra_expr, )*
+
+                (l, r) => unsupported_operation_error!(l, r),
             }
-        }
     };
 
     // Case *without* extra arms (pure numeric)
-    ($fn_name:ident, $intmeth:ident, $floatop:tt) => {
-        define_arithmetic_fn!($fn_name, $intmeth, $floatop, );
+    ($fn_name:expr, $left: expr, $right:expr, $infinity_control: expr, $intmeth:ident, $floatop:tt) => {
+        define_arithmetic_fn!($fn_name, $left, $right, $infinity_control, $intmeth, $floatop, )
     };
 }
 
-// These are marker methods to make the macros compatible and produce the result, they do purely nothing 
-
-#[allow(dead_code)]
-fn add(_p0: MascalValue, _p1: MascalValue, _p2: &InfinityControl, ) -> Result<MascalValue, MascalError> {
-    Ok(MascalValue::NULL)
-}
-
-#[allow(dead_code)]
-fn sub(_p0: MascalValue, _p1: MascalValue, _p2: &InfinityControl, ) -> Result<MascalValue, MascalError> {
-    Ok(MascalValue::NULL)
-}
-
-#[allow(dead_code)]
-fn mul(_p0: MascalValue, _p1: MascalValue, _p2: &InfinityControl, ) -> Result<MascalValue, MascalError> {
-    Ok(MascalValue::NULL)
-}
-
-define_arithmetic_fn!(add, add, +,
-    (MascalValue::String(l), MascalValue::String(r)) => {
-        Ok(MascalValue::String(l + r.as_str()))
-    },
-
-    (MascalValue::DynamicArray(l), MascalValue::DynamicArray(r)) => {
-        Ok(MascalValue::DynamicArray(l.into_iter().chain(r.into_iter()).collect()))
-    }
-);
-
-define_arithmetic_fn!(sub, sub, -);
-define_arithmetic_fn!(mul, mul, *);
-
 impl MascalValue {
+    pub fn add(
+        left: MascalValue,
+        right: MascalValue,
+        infinity_control: &InfinityControl
+    ) -> Result<MascalValue, MascalError> {
+        define_arithmetic_fn!(Self::add, left, right, infinity_control, add, +,
+            (MascalValue::String(l), MascalValue::String(r)) => {
+                Ok(MascalValue::String(l + r.as_str()))
+            },
+
+            (MascalValue::DynamicArray(l), MascalValue::DynamicArray(r)) => {
+                Ok(MascalValue::DynamicArray(l.into_iter().chain(r.into_iter()).collect()))
+            }
+        )
+    }
+
+    pub fn sub(
+        left: MascalValue,
+        right: MascalValue,
+        infinity_control: &InfinityControl
+    ) -> Result<MascalValue, MascalError> {
+        define_arithmetic_fn!(Self::sub, left, right, infinity_control, sub, -)
+    }
+
+    pub fn mul(
+        left: MascalValue,
+        right: MascalValue,
+        infinity_control: &InfinityControl
+    ) -> Result<MascalValue, MascalError> {
+        define_arithmetic_fn!(Self::mul, left, right, infinity_control, mul, *)
+    }
+
     pub fn div(
         left: MascalValue,
         right: MascalValue,
@@ -148,7 +142,7 @@ impl MascalValue {
                             source: String::from("Cannot divide by zero")
                         })
                     }
-                    
+
                     Ok(MascalValue::Float(l / r))
                 },
 
