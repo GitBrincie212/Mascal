57f5d5789280fa074880b12a74c08cededc35862
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-16 09:18:00 +0300

    added the execution data argument

--- src/runtime/variable_table.rs
+++ src/runtime/variable_table.rs
@@ -1,32 +1,37 @@
+use std::borrow::Cow;
 use std::collections::HashMap;
-use std::rc::Rc;
 use std::sync::Arc;
 use crate::defs::blocks::ExecutionBlock;
 use crate::defs::declerations::MascalVariableInitialDeclaration;
 use crate::defs::errors::{MascalError, MascalErrorType};
-use crate::defs::types::MascalUnprocessedType;
+use crate::defs::InfinityControl;
+use crate::defs::types::{MascalType};
 use crate::runtime::execute_expression::execute_expression;
+use crate::runtime::ExecutionData;
 use crate::runtime::values::MascalValue;
 
 #[allow(dead_code)]
 pub type VariableTable = HashMap<String, VariableData>;
 
 pub struct VariableData {
-    pub value: Option<Rc<MascalValue>>,
+    pub value: Option<MascalValue>,
     pub is_constant: bool,
     pub is_nullable: bool,
-    pub atomic_variable_type: Arc<MascalUnprocessedType>
+    pub atomic_variable_type: Arc<MascalType>
 }
 
 #[allow(dead_code)]
 fn create_variable_table_for_type(
     variable_type: Vec<MascalVariableInitialDeclaration>,
     mut table: VariableTable,
-    target_type: Arc<MascalUnprocessedType>,
+    target_type: Arc<MascalType>,
 ) -> Result<VariableTable, MascalError> {
     for var in variable_type {
-        let value: Option<Rc<MascalValue>> = if let Some(unwrapped_val) = var.initial_value {
-            let val: MascalValue = execute_expression(unwrapped_val)?;
+        let value: Option<MascalValue> = if let Some(unwrapped_val) = var.initial_value {
+            let val: Cow<MascalValue> = execute_expression(unwrapped_val, &ExecutionData {
+                variable_table: Some(&table),
+                infinity_control: InfinityControl::AllowInfinity
+            })?;
             if val.is_atomic_type_of(&target_type) {
                 return Err(MascalError {
                     error_type: MascalErrorType::RuntimeError,
@@ -35,7 +40,7 @@ fn create_variable_table_for_type(
                     source: String::from("Interpreted value does not match its type")
                 });
             }
-            Some(Rc::new(val))
+            Some(val.into_owned())
         } else {None};
         table.insert(var.name, VariableData {
             value,
@@ -51,11 +56,11 @@ fn create_variable_table_for_type(
 #[allow(dead_code)]
 pub fn create_variable_table(block: ExecutionBlock) -> Result<VariableTable, MascalError> {
     let mut table: VariableTable = HashMap::new();
-    table = create_variable_table_for_type(block.variables.integers, table, Arc::new(MascalUnprocessedType::Integer))?;
-    table = create_variable_table_for_type(block.variables.floats, table, Arc::new(MascalUnprocessedType::Float))?;
-    table = create_variable_table_for_type(block.variables.strings, table, Arc::new(MascalUnprocessedType::String))?;
-    table = create_variable_table_for_type(block.variables.booleans, table, Arc::new(MascalUnprocessedType::Boolean))?;
-    table = create_variable_table_for_type(block.variables.dynamics, table, Arc::new(MascalUnprocessedType::Dynamic))?;
-    table = create_variable_table_for_type(block.variables.types, table, Arc::new(MascalUnprocessedType::Type))?;
+    table = create_variable_table_for_type(block.variables.integers, table, Arc::new(MascalType::Integer))?;
+    table = create_variable_table_for_type(block.variables.floats, table, Arc::new(MascalType::Float))?;
+    table = create_variable_table_for_type(block.variables.strings, table, Arc::new(MascalType::String))?;
+    table = create_variable_table_for_type(block.variables.booleans, table, Arc::new(MascalType::Boolean))?;
+    table = create_variable_table_for_type(block.variables.dynamics, table, Arc::new(MascalType::Dynamic))?;
+    table = create_variable_table_for_type(block.variables.types, table, Arc::new(MascalType::Type))?;
     Ok(table)
 }
