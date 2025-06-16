8f8a35a821fc8d322dbadb526b6301850b98b058
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-16 09:11:12 +0300

    Added a check to ensure parameters are not initialized with any value via assignment

--- src/semantic_analysis/check_parameters_declaration.rs
+++ src/semantic_analysis/check_parameters_declaration.rs
@@ -3,13 +3,18 @@ use crate::defs::blocks::{ExecutionBlock, MascalParameter};
 use crate::defs::errors::{MascalError, MascalErrorType};
 
 pub fn check_for_param_declaration(function_block: &ExecutionBlock, params: &Vec<MascalParameter>) -> Result<(), MascalError> {
-    let varnames: HashSet<String> = HashSet::from_iter(
-        function_block
-            .variables
-            .iter_all()
-            .iter()
-            .map(|var| var.name.to_string())
-    );
+    let mut varnames: HashSet<&String> = HashSet::new();
+    for var in function_block.variables.iter_all() {
+        if var.initial_value.is_some() {
+            return Err(MascalError {
+                character: 0,
+                line: 0,
+                error_type: MascalErrorType::ParserError,
+                source: String::from("Cannot define an initial value for a parameter inside a function"),
+            })
+        }
+        varnames.insert(&var.name);
+    }
     for param in params {
         if !varnames.contains(&param.name) {
             return Err(MascalError {
