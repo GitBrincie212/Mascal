24329e9b01798cb2f9371e082b8ec2a0972c2eab
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-16 21:48:37 +0300

    Added actual interpretation of code and made the rust code more idiomatic

--- src/runtime.rs
+++ src/runtime.rs
@@ -8,22 +8,34 @@ pub mod values;
 mod value_operations;
 mod execute_statement;
 
+use std::borrow::Cow;
+use std::rc::Rc;
 use crate::ast::AbstractSyntaxTree;
 use crate::defs::blocks::{ExecutionBlock, ScopedBlocks};
 use crate::defs::errors::MascalError;
 use crate::defs::InfinityControl;
-use crate::runtime::variable_table::VariableTable;
+use crate::runtime::execute_statement::execute_statement;
+use crate::runtime::variable_table::{create_variable_table, VariableTable};
 
 pub struct ExecutionData<'a> {
     pub variable_table: Option<&'a VariableTable>,
     pub infinity_control: InfinityControl,
+    pub scoped_blocks: Rc<Vec<ScopedBlocks>>
 }
 
-pub fn interpert(abstract_syntax_tree: AbstractSyntaxTree) -> Result<(), MascalError> {
-    let program_block: &ScopedBlocks = &abstract_syntax_tree.blocks[abstract_syntax_tree.program_index];
-    let exec_block: &ExecutionBlock = match program_block {
+pub fn interpert(mut abstract_syntax_tree: AbstractSyntaxTree) -> Result<(), MascalError> {
+    let mut scoped_blocks: Vec<ScopedBlocks> = abstract_syntax_tree.blocks;
+    let program_block: ScopedBlocks = scoped_blocks.remove(abstract_syntax_tree.program_index);
+    let exec_block: ExecutionBlock = match program_block {
         ScopedBlocks::PROGRAM(exec_block) => exec_block,
-        ScopedBlocks::FUNCTION {..} => {return Ok(())}, // Bored to point out as error
+        ScopedBlocks::FUNCTION {..} => {unreachable!()},
     };
+    let mut scoped_variable_table: VariableTable = create_variable_table(&exec_block)?;
+    let containerized_scoped_blocks: Rc<Vec<ScopedBlocks>> =  Rc::new(scoped_blocks);
+    for statement in &exec_block.body {
+        scoped_variable_table = execute_statement(
+            Cow::Borrowed(statement), scoped_variable_table, containerized_scoped_blocks.clone()
+        )?;
+    }
     Ok(())
 }
