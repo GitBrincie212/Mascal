4aef764cc123fe21d6c1ef5e13b2422a32442cc4
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-16 09:15:48 +0300

    Added a execute_statement module which is WIP and added execution data which stores common data needed for execution on statements, expressions... etc

--- src/runtime.rs
+++ src/runtime.rs
@@ -6,10 +6,18 @@ mod execute_unary_expression;
 mod execute_function_expression;
 pub mod values;
 mod value_operations;
+mod execute_statement;
 
 use crate::ast::AbstractSyntaxTree;
 use crate::defs::blocks::{ExecutionBlock, ScopedBlocks};
 use crate::defs::errors::MascalError;
+use crate::defs::InfinityControl;
+use crate::runtime::variable_table::VariableTable;
+
+pub struct ExecutionData<'a> {
+    pub variable_table: Option<&'a VariableTable>,
+    pub infinity_control: InfinityControl,
+}
 
 pub fn interpert(abstract_syntax_tree: AbstractSyntaxTree) -> Result<(), MascalError> {
     let program_block: &ScopedBlocks = &abstract_syntax_tree.blocks[abstract_syntax_tree.program_index];
