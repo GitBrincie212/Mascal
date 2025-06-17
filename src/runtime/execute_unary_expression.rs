4ee073856eb04cab0bf17d84c1adc7cf2da03c41
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-17 21:28:58 +0300

    Finished the execution of unary expressions

--- src/runtime/execute_unary_expression.rs
+++ src/runtime/execute_unary_expression.rs
@@ -1,13 +1,22 @@
 use std::borrow::Cow;
 use crate::defs::errors::MascalError;
 use crate::defs::expressions::MascalExpression;
-use crate::defs::operators::MascalUnaryOperators;
+use crate::defs::operators::{MascalUnaryOperators};
+use crate::runtime::execute_expression::execute_expression;
 use crate::runtime::ExecutionData;
 use crate::runtime::values::MascalValue;
 
 #[allow(dead_code)]
 pub fn execute_unary_expression<'a>(
-    left: Box<MascalExpression>, operator: MascalUnaryOperators, exec_data: &ExecutionData<'a>
+    target: Box<MascalExpression>, operator: MascalUnaryOperators, exec_data: &ExecutionData<'a>
 ) -> Result<Cow<'a, MascalValue>, MascalError> {
-    todo!()
+    let target_value: MascalValue = execute_expression(*target, exec_data)?.into_owned();
+
+    Ok(Cow::Owned(match operator {
+        MascalUnaryOperators::Not => {MascalValue::not(target_value)}
+        MascalUnaryOperators::Minus => {MascalValue::negate(&target_value)}
+        MascalUnaryOperators::Typeof => {
+            Ok(MascalValue::Type(target_value.as_mascal_type()?))
+        }
+    }?))
 }
