0895a9b2f05cf3857d3db39cea5be03878443813
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-18 17:31:23 +0300

    Changed the way the variable table is created to be more efficient

--- src/runtime/variable_table.rs
+++ src/runtime/variable_table.rs
@@ -1,10 +1,8 @@
-use std::borrow::Cow;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 use std::sync::Arc;
 use crate::defs::blocks::ExecutionBlock;
-use crate::defs::declerations::MascalVariableInitialDeclaration;
 use crate::defs::errors::{MascalError, MascalErrorType};
 use crate::defs::types::{MascalType};
 use crate::runtime::execute_expression::execute_expression;
@@ -25,83 +23,86 @@ pub struct VariableData {
     pub value: Option<Rc<RefCell<MascalValue>>>,
     pub is_constant: bool,
     pub is_nullable: bool,
-    pub array_dimensions: Arc<Vec<usize>>,
-    pub is_dynamic_array: Arc<Vec<bool>>,
+    pub array_dimensions: Rc<[usize]>,
+    pub is_dynamic_array: Rc<[bool]>,
     pub atomic_variable_type: Arc<MascalType>,
 }
 
-#[allow(dead_code)]
-fn create_variable_table_for_type(
-    variable_type: Cow<Vec<MascalVariableInitialDeclaration>>,
-    table: Rc<RefCell<VariableTable>>,
-    target_type: Arc<MascalType>,
-) -> Result<Rc<RefCell<VariableTable>>, MascalError> {
-    for var in variable_type.into_owned() {
-        let value: Option<Rc<RefCell<MascalValue>>> = if let Some(unwrapped_val) = var.initial_value {
-            let val: MascalValue = execute_expression(unwrapped_val, Rc::new(RefCell::new(ExecutionData {
-                variable_table: Some(table.clone()),
-                scoped_blocks: Rc::new(RefCell::new(Vec::new()))
-            })))?;
-            if !val.is_atomic_type_of(&target_type) {
-                return Err(MascalError {
-                    error_type: MascalErrorType::RuntimeError,
-                    line: 0,
-                    character: 0,
-                    source: format!("Evaluated value does not match its atomic type which is {:?}", &target_type)
+macro_rules! create_variable_table_for_type {
+    ($variable_type: expr, $table: expr, $target_type: expr) => {
+        for var in $variable_type {
+            let value: Option<Rc<RefCell<MascalValue>>> = if let Some(unwrapped_val) = var.initial_value {
+                let val: MascalValue = execute_expression(unwrapped_val, Rc::new(RefCell::new(ExecutionData {
+                    variable_table: Some($table.clone()),
+                    scoped_blocks: Rc::new(RefCell::new(Vec::new()))
+                })))?;
+                if !val.is_atomic_type_of(&*$target_type.clone()) {
+                    return Err(MascalError {
+                        error_type: MascalErrorType::RuntimeError,
+                        line: 0,
+                        character: 0,
+                        source: format!("Evaluated value does not match its atomic type which is {:?}", $target_type.clone())
+                    });
+                }
+                Some(Rc::new(RefCell::new(val)))
+            } else {None};
+            let mut dimensions_val: Vec<usize> = Vec::new();
+            for dimension in var.dimensions {
+                let val: MascalValue = execute_expression(dimension, Rc::new(RefCell::new(ExecutionData {
+                    variable_table: Some($table.clone()),
+                    scoped_blocks: Rc::new(RefCell::new(Vec::new()))
+                })))?;
+                
+                dimensions_val.push(match val {
+                    MascalValue::Integer(i) => {
+                        if i.is_negative_or_zero() {
+                            return Err(MascalError {
+                                error_type: MascalErrorType::TypeError,
+                                line: 0,
+                                character: 0,
+                                source: format!("Evaluated expression is not of type integer to be used {:?}", $target_type.clone())
+                            });
+                        }
+                        i.to_i128() as usize
+                    }
+                    _ => {return Err(MascalError {
+                        error_type: MascalErrorType::TypeError,
+                        line: 0,
+                        character: 0,
+                        source: format!("Evaluated expression is not of type integer to be used {:?}", $target_type.clone())
+                    })}
                 });
             }
-            Some(Rc::new(RefCell::new(val)))
-        } else {None};
-        let mut dimensions_val: Vec<usize> = Vec::new();
-        for dimension in var.dimensions {
-            let val: MascalValue = execute_expression(dimension, Rc::new(RefCell::new(ExecutionData {
-                variable_table: Some(table.clone()),
-                scoped_blocks: Rc::new(RefCell::new(Vec::new()))
-            })))?;
             
-            dimensions_val.push(match val {
-                MascalValue::Integer(i) => {
-                    if i.is_negative_or_zero() {
-                        return Err(MascalError {
-                            error_type: MascalErrorType::TypeError,
-                            line: 0,
-                            character: 0,
-                            source: format!("Evaluated expression is not of type integer to be used {:?}", &target_type)
-                        });
-                    }
-                    i.to_i128() as usize
-                }
-                _ => {return Err(MascalError {
-                    error_type: MascalErrorType::TypeError,
-                    line: 0,
-                    character: 0,
-                    source: format!("Evaluated expression is not of type integer to be used {:?}", &target_type)
-                })}
+            $table.borrow_mut().insert(var.name, VariableData {
+                value,
+                is_dynamic_array: Rc::from(var.is_dynamic_array),
+                array_dimensions: Rc::from(dimensions_val),
+                is_constant: var.is_constant,
+                is_nullable: var.is_nullable,
+                atomic_variable_type: $target_type.clone(),
             });
         }
-        
-        table.borrow_mut().insert(var.name, VariableData {
-            value,
-            is_dynamic_array: Arc::new(var.is_dynamic_array),
-            array_dimensions: Arc::new(dimensions_val),
-            is_constant: var.is_constant,
-            is_nullable: var.is_nullable,
-            atomic_variable_type: Arc::clone(&target_type),
-        });
-    }
-    
-    Ok(table)
+    };
 }
 
 #[allow(dead_code)]
-pub fn create_variable_table(block: &ExecutionBlock) -> Result<Rc<RefCell<VariableTable>>, MascalError> {
-    let mut table: Rc<RefCell<VariableTable>> = Rc::new(RefCell::new(HashMap::new()));
+pub fn create_variable_table(mut block: ExecutionBlock) -> Result<(Rc<RefCell<VariableTable>>, ExecutionBlock), MascalError> {
+    let table: Rc<RefCell<VariableTable>> = Rc::new(RefCell::new(HashMap::new()));
 
-    table = create_variable_table_for_type(Cow::Borrowed(&block.variables.integers), table, Arc::new(MascalType::Integer))?;
-    table = create_variable_table_for_type(Cow::Borrowed(&block.variables.floats), table, Arc::new(MascalType::Float))?;
-    table = create_variable_table_for_type(Cow::Borrowed(&block.variables.strings), table, Arc::new(MascalType::String))?;
-    table = create_variable_table_for_type(Cow::Borrowed(&block.variables.booleans), table, Arc::new(MascalType::Boolean))?;
-    table = create_variable_table_for_type(Cow::Borrowed(&block.variables.dynamics), table, Arc::new(MascalType::Dynamic))?;
-    table = create_variable_table_for_type(Cow::Borrowed(&block.variables.types), table, Arc::new(MascalType::Type))?;
-    Ok(table)
+    let integers = std::mem::take(&mut block.variables.integers);
+    let floats = std::mem::take(&mut block.variables.floats);
+    let strings = std::mem::take(&mut block.variables.strings);
+    let booleans = std::mem::take(&mut block.variables.booleans);
+    let dynamics = std::mem::take(&mut block.variables.dynamics);
+    let types = std::mem::take(&mut block.variables.types);
+    
+    create_variable_table_for_type!(integers, Rc::clone(&table), Arc::new(MascalType::Integer));
+    create_variable_table_for_type!(floats, Rc::clone(&table), Arc::new(MascalType::Float));
+    create_variable_table_for_type!(strings, Rc::clone(&table), Arc::new(MascalType::String));
+    create_variable_table_for_type!(booleans, Rc::clone(&table), Arc::new(MascalType::Boolean));
+    create_variable_table_for_type!(dynamics, Rc::clone(&table), Arc::new(MascalType::Dynamic));
+    create_variable_table_for_type!(types, Rc::clone(&table), Arc::new(MascalType::Type));
+    
+    Ok((table, block))
 }
