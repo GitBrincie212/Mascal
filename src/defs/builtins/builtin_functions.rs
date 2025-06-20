76b1d22760d24dca95eee594c768a508c9898c27
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-20 21:02:35 +0300

    Added a couple of nifty builtin array functions

--- src/defs/builtins/builtin_functions.rs
+++ src/defs/builtins/builtin_functions.rs
@@ -5,23 +5,25 @@ use std::num::{ParseFloatError, ParseIntError};
 use std::rc::Rc;
 use std::sync::{Arc};
 use once_cell::sync::Lazy;
+use crate::defs::builtins::utils::flatten_impl;
 use crate::defs::dynamic_int::IntegerNum;
 use crate::defs::errors::{MascalError, MascalErrorType};
 use crate::defs::expressions::MascalExpression;
-use crate::defs::types::MascalType;
+use crate::defs::types::{MascalType, MascalTypeKind};
 use crate::runtime::ExecutionData;
 use crate::runtime::values::MascalValue;
+use crate::runtime::utils::{get_dimensions, get_sizes};
 
 #[derive(Clone)]
 pub enum BuiltinFunction {
     ValueBased {
-        fixed_argument_types: Vec<Vec<MascalType>>,
+        fixed_argument_types: Vec<Vec<MascalTypeKind>>,
         supports_dynamic_arguments: bool,
         execution: Arc<dyn Fn(Vec<MascalValue>, Rc<RefCell<ExecutionData>>) -> Result<Option<MascalValue>, MascalError> + Send + Sync>,
     },
 
     ExpressionBased {
-        fixed_argument_types: Vec<Vec<MascalType>>,
+        fixed_argument_types: Vec<Vec<MascalTypeKind>>,
         supports_dynamic_arguments: bool,
         execution: Arc<dyn Fn(Vec<&MascalExpression>, Rc<RefCell<ExecutionData>>) -> Result<Option<MascalValue>, MascalError> + Send + Sync>,
     }
@@ -30,7 +32,7 @@ pub enum BuiltinFunction {
 impl BuiltinFunction {
     fn new_value_based(
         supports_dynamic_arguments: bool,
-        fixed_argument_types: Vec<Vec<MascalType>>,
+        fixed_argument_types: Vec<Vec<MascalTypeKind>>,
         execution: Arc<dyn Fn(Vec<MascalValue>, Rc<RefCell<ExecutionData>>) -> Result<Option<MascalValue>, MascalError> + Send + Sync>,
     ) -> Self {
         BuiltinFunction::ValueBased {
@@ -42,7 +44,7 @@ impl BuiltinFunction {
 
     fn new_expresion_based (
         supports_dynamic_arguments: bool,
-        fixed_argument_types: Vec<Vec<MascalType>>,
+        fixed_argument_types: Vec<Vec<MascalTypeKind>>,
         execution: Arc<dyn Fn(Vec<&MascalExpression>, Rc<RefCell<ExecutionData>>) -> Result<Option<MascalValue>, MascalError> + Send + Sync>,
     ) -> Self {
         BuiltinFunction::ExpressionBased {
@@ -54,7 +56,10 @@ impl BuiltinFunction {
 }
 
 macro_rules! define_builtin_function {
-    ($builtin_construct: expr, $name: expr, $map: expr, $fixed_args: expr, $supports_dynamic_arguments: expr, $func: expr) => {
+    (
+        $builtin_construct: expr, $name: expr, $map: expr, $fixed_args: expr,
+        $supports_dynamic_arguments: expr, $func: expr
+    ) => {
         let func: Arc<BuiltinFunction> = Arc::new($builtin_construct(
             $supports_dynamic_arguments,
             $fixed_args,
@@ -80,7 +85,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Ln", map, vec![vec![MascalType::Float]], false,
+        BuiltinFunction::new_value_based, "Ln", map, vec![vec![MascalTypeKind::Float]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(MascalValue::Integer(i.ln()?))),
@@ -91,7 +96,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Sqrt", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Sqrt", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(MascalValue::Integer(i.isqrt()?))),
@@ -102,7 +107,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Log", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Log", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(MascalValue::Integer(i.log10()?))),
@@ -113,7 +118,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Log2", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Log2", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(MascalValue::Integer(i.log2()?))),
@@ -124,7 +129,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Exp", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Exp", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(
@@ -137,7 +142,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Cbrt", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Cbrt", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(
@@ -150,7 +155,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Abs", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Abs", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(
@@ -163,7 +168,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Sin", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Sin", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(
@@ -176,7 +181,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Tan", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Tan", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(
@@ -189,7 +194,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Cos", map, vec![vec![MascalType::Float, MascalType::Integer]], false,
+        BuiltinFunction::new_value_based, "Cos", map, vec![vec![MascalTypeKind::Float, MascalTypeKind::Integer]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Integer(i) => Ok(Some(
@@ -202,7 +207,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Floor", map, vec![vec![MascalType::Float]], false,
+        BuiltinFunction::new_value_based, "Floor", map, vec![vec![MascalTypeKind::Float]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Float(f) => Ok(Some(MascalValue::Float(f.floor()))),
@@ -212,7 +217,7 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Ceil", map, vec![vec![MascalType::Float]], false,
+        BuiltinFunction::new_value_based, "Ceil", map, vec![vec![MascalTypeKind::Float]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Float(f) => Ok(Some(MascalValue::Float(f.ceil()))),
@@ -222,12 +227,164 @@ pub static BUILT_IN_FUNCTION_TABLE: Lazy<HashMap<String, Arc<BuiltinFunction>>> 
     );
 
     define_builtin_function!(
-        BuiltinFunction::new_value_based, "Round", map, vec![vec![MascalType::Float]], false,
+        BuiltinFunction::new_value_based, "Round", map, vec![vec![MascalTypeKind::Float]], false,
         |args, _| {
             match args.first().unwrap() {
                 MascalValue::Float(f) => Ok(Some(MascalValue::Float(f.round()))),
                 _ => unreachable!()
             }
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Len", map,
+        vec![vec![MascalTypeKind::StaticArray, MascalTypeKind::DynamicArray]],
+        false,
+        |args, _exec_data| {
+            let length: usize = match args.first().unwrap() {
+                MascalValue::StaticArray(v) => v.len(),
+                MascalValue::DynamicArray(v) => v.len(),
+                _ => {unreachable!()}
+            };
+            Ok(Some(MascalValue::Integer(IntegerNum::new(length as i128))))
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Dimensions", map,
+        vec![vec![MascalTypeKind::StaticArray, MascalTypeKind::DynamicArray]],
+        false,
+        |args, _exec_data| {
+            get_dimensions(&Some(args.first().unwrap().clone()), 0)
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Sizes", map,
+        vec![vec![MascalTypeKind::StaticArray, MascalTypeKind::DynamicArray]],
+        false,
+        |args, _exec_data| {
+            get_sizes(&Some(args.first().unwrap().clone()), Vec::new())
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Push", map,
+        vec![vec![MascalTypeKind::DynamicArray]],
+        true,
+        |args, _exec_data| {
+            let array: MascalValue = args[0].clone();
+            if args.len() == 1 {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ArgumentError,
+                    line: 0,
+                    character: 0,
+                    source: String::from("Expected a value (element) to be pushed to the array")
+                });
+            }
+            if let MascalValue::DynamicArray(mut v) = array {
+                v.reserve(args.len());
+                for val in args.into_iter().skip(1) {
+                    v.push(Rc::new(RefCell::new(Some(val))));
+                }
+                return Ok(Some(MascalValue::DynamicArray(v)));
+            }
+            unreachable!()
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Append", map,
+        vec![vec![MascalTypeKind::DynamicArray]],
+        true,
+        |args, _exec_data| {
+            let array: MascalValue = args[0].clone();
+            if args.len() == 1 {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ArgumentError,
+                    line: 0,
+                    character: 0,
+                    source: String::from("Expected an array of elements to be appended to the array")
+                });
+            }
+            if let MascalValue::DynamicArray(mut v) = array {
+                let mut counter: usize = 0;
+                for arg in args.iter().skip(1) {
+                    counter += match arg {
+                        MascalValue::StaticArray(v) => v.len(),
+                        MascalValue::DynamicArray(v) => v.len(),
+                        _ => {
+                            return Err(MascalError {
+                                error_type: MascalErrorType::ArgumentError,
+                                line: 0,
+                                character: 0,
+                                source: String::from("Expected an array to push but got an atomic type")
+                            });
+                        }
+                    };
+                }
+                v.reserve(counter);
+                for arr_val in args.into_iter().skip(1) {
+                    match arr_val {
+                        MascalValue::DynamicArray(v_inner) => {
+                            for val in v_inner {
+                                v.push(Rc::new(RefCell::new(val.borrow().clone())));
+                            }
+                        }
+                        _ => unreachable!()
+                    }
+                }
+                return Ok(Some(MascalValue::DynamicArray(v)));
+            }
+            unreachable!()
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Flatten", map,
+        vec![vec![MascalTypeKind::DynamicArray]],
+        false,
+        |args, _exec_data| {
+            let mut flat_cells = Vec::new();
+
+            for arg in args.into_iter() {
+                let cell = Rc::new(RefCell::new(Some(arg)));
+                flat_cells.extend(flatten_impl(cell)?);
+            }
+
+            Ok(Some(MascalValue::DynamicArray(flat_cells)))
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Pop", map,
+        vec![vec![MascalTypeKind::DynamicArray]],
+        false,
+        |args, _exec_data| {
+            return match &args[0] {
+                MascalValue::DynamicArray(v) => {
+                    let mut values_clone = v.clone();
+                    values_clone.pop();
+                    Ok(Some(MascalValue::DynamicArray(values_clone)))
+                }
+                _ => unreachable!()
+            };
+        }
+    );
+
+    define_builtin_function!(
+        BuiltinFunction::new_value_based, "Shift", map,
+        vec![vec![MascalTypeKind::DynamicArray]],
+        false,
+        |args, _exec_data| {
+            return match &args[0] {
+                MascalValue::DynamicArray(v) => {
+                    let mut values_clone = v.clone();
+                    values_clone.remove(0);
+                    Ok(Some(MascalValue::DynamicArray(values_clone)))
+                }
+                _ => unreachable!()
+            };
         }
     );
 
