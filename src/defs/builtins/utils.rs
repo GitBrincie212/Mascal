6badfa794a25c061899a56646058e0d2775214a5
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-21 13:27:25 +0300

    made Min and Max less boilerplate

--- src/defs/builtins/utils.rs
+++ src/defs/builtins/utils.rs
@@ -32,3 +32,32 @@ pub fn flatten_impl(
         }
     }
 }
+
+#[macro_export]
+macro_rules! min_max_common_operation {
+    ($args: expr, $operator: ident) => {
+        match ($args.first().unwrap(), $args.last().unwrap()) {
+            (MascalValue::Float(f), MascalValue::Integer(i)) => {
+                return if i.as_f64().$operator(f) {Ok(Some(MascalValue::Float(*f)))} else {
+                    Ok(Some(MascalValue::Integer(i.clone())))
+                }
+            }
+            (MascalValue::Integer(i), MascalValue::Float(f)) => {
+                return if i.as_f64().$operator(f) {Ok(Some(MascalValue::Float(*f)))} else {
+                    Ok(Some(MascalValue::Integer(i.clone())))
+                }
+            }
+            (MascalValue::Float(f1), MascalValue::Float(f2)) => {
+                return if (*f1).$operator(f2) {Ok(Some(MascalValue::Float(*f1)))} else {
+                    Ok(Some(MascalValue::Float(*f2)))
+                }
+            }
+            (MascalValue::Integer(i1), MascalValue::Integer(i2)) => {
+                return if i1.to_i128().$operator(&i2.to_i128()) {Ok(Some(MascalValue::Integer(i1.clone())))} else {
+                    Ok(Some(MascalValue::Integer(i2.clone())))
+                }
+            }
+            (_, _) => unreachable!()
+        }
+    };
+}
