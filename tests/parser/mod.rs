e4ab0e7cd1874e0bcdfeeb6445641bec95838342
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-26 20:11:34 +0300

    Added parser module for parser-related tests

---
+++ tests/parser/mod.rs
@@ -0,0 +1,41 @@
+mod parse_throw_statement;
+
+#[macro_export]
+macro_rules! run_parsing {
+    ($input: expr) => {{
+        use mascal::defs::token::Token;
+        use mascal::lexer;
+        use mascal::parser::TokenSequence;
+        use mascal::parser;
+        
+        let tokens: Vec<Token> = lexer::tokenize($input).unwrap();
+        let token_sequence: TokenSequence = TokenSequence::new(tokens);
+        parser::parse(token_sequence)
+    }};
+}
+
+#[macro_export]
+macro_rules! expect_error {
+    ($vals: expr) => {{
+        let input: String = define_program_boilerplate!(Vec::<String>::new(), $vals);
+        let ast: Result<AbstractSyntaxTree, MascalError> = run_parsing!(input.as_str());
+        assert_eq!(ast.is_err(), true);
+        ast
+    }};
+}
+
+#[macro_export]
+macro_rules! define_program_boilerplate {
+    ($variables: expr, $code: expr) => {{
+        format!("\
+            DEFINE_PROGRAM {{\n \
+                \tVARIABLES {{\n \
+                    \t\t{} \n \
+                \t}} \n \
+                \n \
+                \tIMPLEMENTATION {{\n \
+                    \t\t{}\n \
+                \t}}\n \
+            }}", $variables.join("\n\t\t\t\t\t"), $code.join("\n\t\t\t\t\t"))
+    }};
+}
---
+++ tests/parser/parse_throw_statement.rs
@@ -0,0 +1,242 @@
+use mascal::ast::AbstractSyntaxTree;
+use mascal::defs::blocks::ScopedBlocks;
+use mascal::defs::errors::{MascalError, MascalErrorType};
+use mascal::defs::statements::MascalStatement;
+use crate::{define_program_boilerplate, expect_error, run_parsing};
+
+macro_rules! get_correct_error_inputs {
+    () => {{
+        vec![
+            ("TypeError", MascalErrorType::TypeError),
+            ("RuntimeError", MascalErrorType::RuntimeError),
+            ("OverflowError",  MascalErrorType::OverflowError),
+            ("UndefinedOperationError", MascalErrorType::UndefinedOperation),
+            ("IndexError", MascalErrorType::IndexError),
+            ("InputError", MascalErrorType::InputError),
+            ("ArgumentError",  MascalErrorType::ArgumentError),
+            ("ValueError", MascalErrorType::ValueError)
+        ]
+    }}
+}
+
+macro_rules! define_general_throw_statement_boilerplate {
+    ($func: expr, $throws: expr) => {
+        let errors: Vec<(&str, MascalErrorType)> = get_correct_error_inputs!();
+        for (error_id, expected) in errors {
+            for throw_case in $throws {
+                $func(error_id, throw_case, &expected);
+            }
+        }
+    };
+
+    ($func: expr) => {
+        define_general_throw_statement_boilerplate!($func, vec!["THROW", "throw", "Throw"]);
+    }
+}
+
+#[test]
+fn test_correct_parsing() {
+    define_general_throw_statement_boilerplate!(|error_id, throw_case, expected| {
+        let input: String = define_program_boilerplate!(Vec::<String>::new(), vec![
+                format!("{throw_case} {error_id}: \"test Test TEST!\";")
+            ]);
+            let ast: AbstractSyntaxTree = run_parsing!(input.as_str())
+                .unwrap(); // For better error handling
+            assert_eq!(ast.blocks.len(), 1);
+            let ScopedBlocks::Program(exec) = &ast.blocks[0] else {unreachable!()};
+            assert_eq!(exec.body.len(), 1);
+            assert_eq!(exec.variables.iter_all().iter().count(), 0);
+            assert!(
+                matches!(&exec.body[0],
+                    MascalStatement::Throw {
+                        error_type,
+                        message,
+                    } if error_type == expected && message == "test Test TEST!"
+                ),
+                "got {:?}, expected Throw {{ error_type: {:?}, message: {:?} }}",
+                exec.body[0], expected, "test Test TEST!"
+            );
+    });
+}
+
+#[test]
+fn test_incorrect_parsing1() {
+    let errors: Vec<&str> = vec![
+        "TypeErRor",
+        "RuntImeError",
+        "OVerfloWError",
+        "UnDEfinedOPErationERRor",
+        "IndexErro",
+        "nputError",
+        "ArgumentRror",
+        "Value",
+    ];
+    for error_id in errors {
+        for throw_case in vec!["THROW", "throw", "Throw"] {
+            let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("{throw_case} {error_id}: \"test Test TEST!\";")
+            ]);
+            assert!(
+                matches!(ast.as_ref().unwrap_err(),
+                    MascalError {
+                        error_type,
+                        source,
+                        ..
+                    } if *error_type == MascalErrorType::UndefinedErrorType
+                        && source == "Use of an undefined usable error type in the throw statement (perhaps a typo?)"
+                ),
+                "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+                &ast, MascalErrorType::UndefinedErrorType, "test Test TEST!"
+            );
+        }
+    }
+}
+
+#[test]
+fn test_incorrect_parsing2() {
+    define_general_throw_statement_boilerplate!(|error_id, throw_case, _| {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("{throw_case} {error_id} \"test Test TEST!\";")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Expected a colon for the throw statement but got \"test Test TEST!\""
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Expected a colon for the throw statement but got <...>"
+        );
+    });
+}
+
+#[test]
+fn test_incorrect_parsing3() {
+    define_general_throw_statement_boilerplate!(|error_id, throw_case, _| {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("{throw_case} {error_id} ;")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Expected a colon for the throw statement but got nothing"
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Expected a colon for the throw statement but got nothing"
+        );
+    });
+}
+
+#[test]
+fn test_incorrect_parsing4() {
+    define_general_throw_statement_boilerplate!(|error_id, throw_case, _| {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("{throw_case}: {error_id};")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Expected a error type to throw but got \":\""
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Expected a error type to throw but got \":\""
+        );
+    });
+}
+
+#[test]
+fn test_incorrect_parsing5() {
+    define_general_throw_statement_boilerplate!(|error_id, throw_case, _| {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("{throw_case} {error_id};")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Expected a colon for the throw statement but got nothing"
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Expected a colon for the throw statement but got nothing"
+        );
+    });
+}
+
+#[test]
+fn test_incorrect_parsing6() {
+    define_general_throw_statement_boilerplate!(|error_id, throw_case, _| {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("{throw_case} {error_id}:;")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Expected a message for the throw statement but got nothing"
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Expected a message for the throw statement but got nothing"
+        );
+    });
+}
+
+#[test]
+fn test_incorrect_parsing7() {
+    let errors: Vec<(&str, MascalErrorType)> = get_correct_error_inputs!();
+    for (error_id, _) in errors {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("ThrOW {error_id}: \"12345\";")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Unexpected character sequences found in a supposed expression"
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Unexpected character sequences found in a supposed expression"
+        );
+    }
+}
+
+#[test]
+fn test_incorrect_parsing8() {
+    let errors: Vec<(&str, MascalErrorType)> = get_correct_error_inputs!();
+    for (error_id, _) in errors {
+        let ast: Result<AbstractSyntaxTree, MascalError> = expect_error!(vec![
+                format!("THROW {error_id}: \"12345\"")
+        ]);
+        assert!(
+            matches!(ast.as_ref().unwrap_err(),
+                MascalError {
+                    error_type,
+                    source,
+                    ..
+                } if *error_type == MascalErrorType::ParserError
+                && source == "Unexpected characters found inside implementation block, perhaps forgot a semicolon?"
+            ),
+            "got {:?}, expected MascalError {{ error_type: {:?}, message: {:?}, ... }}",
+            &ast, MascalErrorType::ParserError, "Unexpected characters found inside implementation block, perhaps forgot a semicolon?"
+        );
+    }
+}
