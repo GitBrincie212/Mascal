56345d1be5adb5aece1127735a142e3cd4da818d
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-27 21:13:06 +0300

    Rewrote the parsing of index expressions

--- src/parser/parse_expression/parse_indexing.rs
+++ src/parser/parse_expression/parse_indexing.rs
@@ -1,110 +1,80 @@
 use crate::defs::binding_power::BindingPower;
-use crate::defs::errors::MascalError;
+use crate::defs::errors::{MascalError, MascalErrorType};
 use crate::defs::expressions::MascalExpression;
 use crate::defs::loop_flags::LoopFlags;
 use crate::defs::token::{Token, TokenType};
 use crate::parser::parse_expression::parse_expression_internal;
 
+macro_rules! parse_inner_expr_of_array {
+    (
+        $lhs: expr, $pos: expr, $tokens: expr, $open_tok: expr,
+        $close_tok: expr, $close_tok_char: expr, $is_dynamic: expr
+    ) => {
+        *$pos += 1;
+        let mut indexing_length: usize = 0;
+        let mut has_found_closing: bool = false;
+        let mut depth = 1;
+        for tok in &$tokens[*$pos..] {
+            indexing_length += 1;
+            match &tok.token_type {
+                tt if *tt == $open_tok => {
+                    depth += 1;
+                }
+                
+                tt if *tt == $close_tok => {
+                    depth -= 1;
+                    if depth == 0 {
+                        has_found_closing = true;
+                        break;
+                    }
+                }
+                
+                _ => {},
+            }
+        }
+        if !has_found_closing {
+            return Err(MascalError {
+                error_type: MascalErrorType::ParserError,
+                line: $tokens[*$pos].line,
+                character: $tokens[*$pos].start,
+                source: format!("Expected {:?} after index expression", $close_tok_char) 
+            });
+        }
+        let idx: MascalExpression = parse_expression_internal(
+            &$tokens[*$pos..*$pos + indexing_length - 1], &mut 0usize, BindingPower::new(0)
+        )?;
+        *$pos += indexing_length;
+        $lhs = MascalExpression::Indexing {
+            array: Box::new($lhs),
+            index: Box::new(idx),
+            is_dynamic: $is_dynamic,
+        };
+    };
+}
+
 pub fn parse_indexing_expression(
     tokens: &[Token],
     pos: &mut usize,
     _min_bp: &BindingPower,
-    lhs: MascalExpression,
+    mut lhs: MascalExpression,
 ) -> Result<(LoopFlags, MascalExpression), MascalError> {
-    let tok = tokens.get(*pos).map(|t| &t.token_type);
-    let is_dynamic_array = tok == Some(&TokenType::OpenDynamicArray);
-    if !is_dynamic_array && tok != Some(&TokenType::OpenBracket) {
-        return Ok((LoopFlags::None, lhs));
-    }
-
-    let mut depth = 0;
-    let mut match_idx = None;
-    for (i, tok) in tokens.iter().enumerate().skip(*pos) {
-        match tok.token_type {
-            TokenType::OpenDynamicArray | TokenType::OpenBracket => depth += 1,
-            TokenType::CloseDynamicArray | TokenType::CloseBracket => {
-                depth -= 1;
-                if depth == 0 {
-                    match_idx = Some(i);
-                    break;
-                }
+    loop {
+        match tokens.get(*pos).map(|t| &t.token_type) {
+            Some(TokenType::OpenBracket) => {
+                parse_inner_expr_of_array!(
+                    lhs, pos, tokens, TokenType::OpenBracket, 
+                    TokenType::CloseBracket, "]", false
+                );
             }
-            _ => {}
-        }
-    }
-    let end = if let Some(i) = match_idx {
-        i
-    } else {
-        return Ok((LoopFlags::None, lhs));
-    };
 
-    if tokens
-        .get(end + 1)
-        .map(|t| {
-            matches!(
-                t.token_type,
-                TokenType::Identifier
-                    | TokenType::IntegerLiteral
-                    | TokenType::FloatLiteral
-                    | TokenType::StringLiteral
-                    | TokenType::OpenParen
-                    | TokenType::OpenBracket
-            )
-        })
-        .unwrap_or(false)
-    {
-        return Ok((LoopFlags::None, lhs));
-    }
-
-    let open_tt = if is_dynamic_array {
-        TokenType::OpenDynamicArray
-    } else {
-        TokenType::OpenBracket
-    };
-    let close_tt = if is_dynamic_array {
-        TokenType::CloseDynamicArray
-    } else {
-        TokenType::CloseBracket
-    };
-    let is_dynamic = true;
-
-    *pos += 1;
-    let mut inner_depth = 1;
-    let mut indices = Vec::new();
-    while *pos < tokens.len() {
-        let token = &tokens[*pos];
-        *pos += 1;
-        match &token.token_type {
-            tt if tt == &close_tt => {
-                inner_depth -= 1;
-                if inner_depth == 0 {
-                    break;
-                }
+            Some(TokenType::OpenDynamicArray) => {
+                parse_inner_expr_of_array!(
+                    lhs, pos, tokens, TokenType::OpenDynamicArray, 
+                    TokenType::CloseDynamicArray, ">>", true
+                );
             }
-            tt if tt == &open_tt => inner_depth += 1,
-            _ => {}
-        }
-        if inner_depth > 0 {
-            indices.push(token.clone());
+
+            _ => { return  Ok((LoopFlags::None, lhs))},
         }
     }
-
-    let mut inner_pos: usize = 0;
-    let idx_expr: MascalExpression = parse_expression_internal(
-        &indices,
-        &mut inner_pos,
-        BindingPower {
-            left_binding_power: 0,
-            right_binding_power: 0,
-        },
-    )?;
-
-    Ok((
-        LoopFlags::Continue,
-        MascalExpression::Indexing {
-            array: Box::new(lhs),
-            index: Box::new(idx_expr),
-            is_dynamic,
-        },
-    ))
 }
