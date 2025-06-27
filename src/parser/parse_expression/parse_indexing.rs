11cb3e19279bbc6e3651c62a94a87d7dce79ba42
McBrincie212 <92397968+GitBrincie212@users.noreply.github.com>
2025-06-27 22:06:59 +0300

    Fixed 2 bugs, the first regarding the position overflowing out of the tokens range and the other is encountering a close array character and simply breaking out

--- src/parser/parse_expression/parse_indexing.rs
+++ src/parser/parse_expression/parse_indexing.rs
@@ -14,13 +14,21 @@ macro_rules! parse_inner_expr_of_array {
         let mut indexing_length: usize = 0;
         let mut has_found_closing: bool = false;
         let mut depth = 1;
+        if *$pos >= $tokens.len() {
+            return Err(MascalError {
+                error_type: MascalErrorType::ParserError,
+                line: $tokens.last().unwrap().line,
+                character: $tokens.last().unwrap().start,
+                source: format!("Expected {:?} after index expression", $close_tok_char)
+            });
+        }
         for tok in &$tokens[*$pos..] {
             indexing_length += 1;
             match &tok.token_type {
                 tt if *tt == $open_tok => {
                     depth += 1;
                 }
-                
+
                 tt if *tt == $close_tok => {
                     depth -= 1;
                     if depth == 0 {
@@ -28,7 +36,7 @@ macro_rules! parse_inner_expr_of_array {
                         break;
                     }
                 }
-                
+
                 _ => {},
             }
         }
@@ -37,7 +45,7 @@ macro_rules! parse_inner_expr_of_array {
                 error_type: MascalErrorType::ParserError,
                 line: $tokens[*$pos].line,
                 character: $tokens[*$pos].start,
-                source: format!("Expected {:?} after index expression", $close_tok_char) 
+                source: format!("Expected {:?} after index expression", $close_tok_char)
             });
         }
         let idx: MascalExpression = parse_expression_internal(
@@ -59,19 +67,38 @@ pub fn parse_indexing_expression(
     mut lhs: MascalExpression,
 ) -> Result<(LoopFlags, MascalExpression), MascalError> {
     loop {
-        match tokens.get(*pos).map(|t| &t.token_type) {
+        let curr: Option<&Token> = tokens.get(*pos);
+        match curr.map(|t| &t.token_type) {
             Some(TokenType::OpenBracket) => {
                 parse_inner_expr_of_array!(
-                    lhs, pos, tokens, TokenType::OpenBracket, 
+                    lhs, pos, tokens, TokenType::OpenBracket,
                     TokenType::CloseBracket, "]", false
                 );
             }
 
             Some(TokenType::OpenDynamicArray) => {
                 parse_inner_expr_of_array!(
-                    lhs, pos, tokens, TokenType::OpenDynamicArray, 
+                    lhs, pos, tokens, TokenType::OpenDynamicArray,
                     TokenType::CloseDynamicArray, ">>", true
                 );
+            }
+
+            Some(TokenType::CloseBracket) => {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ParserError,
+                    line: curr.unwrap().line,
+                    character: curr.unwrap().start,
+                    source: String::from("Expected an opening character \"[\" before closing an unopened static array")
+                })
+            }
+            
+            Some(TokenType::CloseDynamicArray) => {
+                return Err(MascalError {
+                    error_type: MascalErrorType::ParserError,
+                    line: curr.unwrap().line,
+                    character: curr.unwrap().start,
+                    source: String::from("Expected an opening character \"<<\" before closing an unopened dynamic array")
+                })
             }
 
             _ => { return  Ok((LoopFlags::None, lhs))},
